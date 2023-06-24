0edbc51479826035aba25703f8ac08326c627837
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-06-24 16:01:46 -0700

    Add

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Function that adds two integers
+    Args:
+        a: first number
+        b: second number
+    Return:
+        the result
+    """
+    return (a + b)
