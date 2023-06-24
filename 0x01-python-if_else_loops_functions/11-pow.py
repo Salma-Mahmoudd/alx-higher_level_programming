abafdb4aedbe267ba6af70f5ee91512951d0f4fe
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-06-24 16:10:11 -0700

    Power

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """Function that computes a to the power of b
+    Args:
+        a: first number
+        b: second number
+    Return:
+        the result
+    """
+    return (a ** b)
