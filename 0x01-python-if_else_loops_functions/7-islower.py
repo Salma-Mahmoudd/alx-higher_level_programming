124e4c68c492f3227844109fa6e7b678bda57534
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-06-24 14:56:40 -0700

    checks for lowercase character

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def islower(c):
+    """Function that checks for lowercase character
+    Args:
+        c: char to be checked
+    Return:
+        True or False
+    """
+    if c > 96 and c < 123:
+        return (True)
+    else:
+        return (False)
