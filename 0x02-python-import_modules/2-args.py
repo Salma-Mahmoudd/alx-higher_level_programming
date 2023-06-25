69311646c1b1ada4ef2e2b904afbeead62f83db7
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-06-24 18:06:01 -0700

    list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    print("{} arguments.".format(n))
+    for i in range(n):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
