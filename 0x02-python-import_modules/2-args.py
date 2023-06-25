a9edc497e9fc087c4760fbe9cad404ec91ca635f
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-06-24 18:11:46 -0700

    list of its arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     n = len(sys.argv) - 1
-    print("{} arguments.".format(n))
+    print("{} argument{}{}".format(n, 's' if n != 1 else '', '.' if n == 0 else ':'))
     for i in range(n):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
