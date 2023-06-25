859d99aedd952f16941e2228e08adb63309e4227
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-06-25 16:35:05 -0700

    Update dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    tmp = sorted(a_dictionary.keys())
-    for i in tmp:
-        print("{}: {}".format(i, a_dictionary[i]))
+    if a_dictionary is not None:
+        tmp = sorted(a_dictionary.keys())
+        for i in tmp:
+            print("{}: {}".format(i, a_dictionary[i]))
