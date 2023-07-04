2ea774dc9fe0ccf022e49d449cbe7d94abc09d5d
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-07-03 19:39:10 -0700

    A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -6,12 +6,12 @@ class Rectangle:
     """Define a rectangle with private attributes: width, height"""
 
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
-        Rectangle.number_of_instances += 1
+        Rectangle.number_of_instances = Rectangle.number_of_instances + 1
 
     @property
     def width(self):
