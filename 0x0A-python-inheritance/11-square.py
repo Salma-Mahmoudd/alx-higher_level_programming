6d4dea2c8c2c70dfc560b1c77589baeed54954a0
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-07-12 10:15:10 -0700

    Square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""a class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """size must be private and positive"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
