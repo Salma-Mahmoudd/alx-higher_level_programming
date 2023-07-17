e9292e443787d9e2d03ec63d50b9cb3e012201b7
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-07-17 11:27:44 -0700

    Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,15 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
+        """Initialization attributes of new object.
+
+        Args:
+            width: the width of rec.
+            height: the height of rec.
+            x: the coordinate of rec in x axis
+            y: the coordinate of rec in y axis
+            id: id of instance
+        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
