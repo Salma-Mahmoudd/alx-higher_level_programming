bb4d98d9f7b1b7a37dd18e5d293f1c3571c8e3c6
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-07-17 14:02:49 -0700

    Base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,3 +50,14 @@ class Base:
             inst = cls(1, 1)
         inst.update(**dictionary)
         return inst
+
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        File = str(cls.__name__) + ".json"
+        with open(File, 'r') as jfile:
+            list = []
+            for i in Base.from_json_string(jfile.read()):
+                list.append(cls.create(**i))
+            return list
+        return []
