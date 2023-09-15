3ea034b3e7f21015953c21c8bcff143c1573a62f
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-09-15 15:02:01 -0700

    Arizona column

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,12 +5,12 @@ displays all values in the states table
 of database where name matches the argument.
 """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(argv[4]))
     for i in cur.fetchall():
         print(i)
     cur.close()
