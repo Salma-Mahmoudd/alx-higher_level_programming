970a63607d48fdb5a6fd869bcd34b89daee3dd28
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-09-15 13:41:55 -0700

    List all table order by id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.sursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
     for i in cur.fetchall():
         print(i)
